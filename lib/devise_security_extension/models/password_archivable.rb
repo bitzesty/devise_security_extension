82ee821feacbe5d857ce3595a9385c6fda5ef030
Ruslan Khamidullin <rusllonrails@gmail.com>
2019-01-17 18:13:50 +0300

    [RAILS 5.1] MongoDB fixes

--- lib/devise_security_extension/models/password_archivable.rb
+++ lib/devise_security_extension/models/password_archivable.rb
@@ -25,7 +25,14 @@ module Devise
         end
 
         if self.class.deny_old_passwords > 0 and not self.password.nil?
-          old_passwords_including_cur_change = self.old_passwords.order(:id).reverse_order.limit(self.class.deny_old_passwords).to_a
+          old_passwords_including_cur_change = if self.old_passwords.respond_to?("order_by")
+            # mongodb
+            self.old_passwords.order_by(id: 'desc').limit(self.class.deny_old_passwords).to_a
+          else
+            # active record
+            self.old_passwords.order(:id).reverse_order.limit(self.class.deny_old_passwords).to_a
+          end
+
           old_passwords_including_cur_change << OldPassword.new(old_password_params)  # include most recent change in list, but don't save it yet!
           old_passwords_including_cur_change.each do |old_password|
             dummy                    = self.class.new
@@ -61,7 +68,15 @@ module Devise
         if encrypted_password_changed?
           if archive_count.to_i > 0
             old_passwords.create! old_password_params
-            old_passwords.order(:id).reverse_order.offset(archive_count).destroy_all
+
+            # mongodb
+            if self.old_passwords.respond_to?("order_by")
+              self.old_passwords.order_by(id: 'desc').skip(archive_count).destroy_all
+            else
+              # active record
+              self.old_passwords.order(:id).reverse_order.offset(archive_count).destroy_all
+            end
+
           else
             old_passwords.destroy_all
           end
